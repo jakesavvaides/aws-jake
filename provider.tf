804b6a9cfd1097d31214c148dfaeefd9cd1862c7
fosterm-mw <michael.foster@mavenwave.com>
2022-12-07 14:07:13 -0600

    no string format needed

--- provider.tf
+++ provider.tf
@@ -8,9 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.region}"
+  region = var.region
 }
 
 data "aws_availability_zones" "available" {}
